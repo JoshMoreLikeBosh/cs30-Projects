55a84fa4fca8a2cf00d57134b3cb7a6060fc1599
Bosh <Joshmorelikebosh@gmail.com>
2023-03-07 15:19:57 -0600

    Update sketch.js

--- Doohickey/sketch.js
+++ Doohickey/sketch.js
@@ -1,15 +1,45 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// cubic disarray
 
 
+let boxes = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+
+
+  for (let y=0; y < height; y+=50) {
+    for (let x = 0; x<width; x+=50) {
+      spawnBox(x, y, 50, random(y*mouseY));
+    }
+  }
+    
 }
 
 function draw() {
   background(220);
+
+  //display all boxes
+  for (let i=0; i<boxes.length; i++) {
+    displayBox(boxes[i]);
+  }
+  
+}
+
+function displayBox(myBox) {
+  push(); //saving transformation matrix
+  translate(myBox.x, myBox.y);
+  rotate(myBox.rotation);
+  square(0, 0, myBox.size);
+  pop(); //resetting transformation matrix
+}
+
+function spawnBox(theX, theY, theSize, howRotated) {
+  let someBox = {
+    x: theX,
+    y: theY,
+    size: theSize,
+    rotation: howRotated,
+  };
+  boxes.push(someBox);
 }
