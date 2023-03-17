cc32be7afacd6616170c44eeef7827c2ef8ae40d
Bosh <Joshmorelikebosh@gmail.com>
2023-03-17 14:58:48 -0600

    shape and colors

--- whatchamacallit/sketch.js
+++ whatchamacallit/sketch.js
@@ -1,15 +1,45 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Perlin and Bubbles DEMO
+// 03/17/23
 
+let theBubbles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spawnBubble();
+  noStroke();
+  window.setInterval(spawnBubble, 500);
 }
 
+function mousePressed() {
+  spawnBubble();
+}
+
+
 function draw() {
-  background(220);
+  //background(255);
+
+  //javascript convienience loop
+  for (const bubble of theBubbles) {
+    //move
+    bubble.x = noise(bubble.time) * width;
+    bubble.y = noise(bubble.time+300) * height;
+
+    //display
+    fill(bubble.color);
+    circle(bubble.x, bubble.y, bubble.size);
+
+    //update time
+    bubble.time += 0.01;
+  }
+}
+
+function spawnBubble() {
+  let bubble = {
+    x: random(width),
+    y: random(height),
+    size: random(5, 50),
+    color: color(random(255), random(255), random(255), random(255)),
+    time: random(10000),
+  };
+  theBubbles.push(bubble);
 }
