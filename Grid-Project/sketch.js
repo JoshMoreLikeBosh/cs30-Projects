684bc499cfce661fb9361a7606797643daf9d0bb
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-04-06 13:04:23 -0600

    ngfgnxfgn

--- Grid-Project/sketch.js
+++ Grid-Project/sketch.js
@@ -2,21 +2,33 @@
 // Dosh
 // March 23
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// All Controls (theres alot):
+// w = pan cam up
+// a = pan cam left
+// s = pan cam down
+// d = pan cam right
+// q = move cam forward
+// e = move cam back
+// t, f, g, h = rotate cubes
+// k = shrink
+// l = expand
+// z = reset cubes to default
+// arrowkeys = tilt cam in direction, like turning head
 
 let gridSize = 20;
 let cam;
-let delta = 0;
 let grid;
 const ROWS = gridSize;
 const COLS = gridSize;
 let boxSize = 60;
 let boxGap = -60;
 let images;
-let boxStuff = {};
-let imagenum = prompt("image number 0 to 8");
-let SkyBoxnum = prompt("whats the skybox 0 - 9");
+let imagenum = prompt("what's the box texture! 0 to 9");
+let SkyBoxnum = prompt("what's the skybox 0 - 9");
+let backgroundColor = prompt("what should the background color be?")
+let shapeType;
+let theShapenum = prompt("whats the shape? 0 - 5");
+let stuff;
 
 function preload() {
   images = [];
@@ -30,7 +42,22 @@ function preload() {
   images.push(loadImage("skry.png")); // 7
   images.push(loadImage("sky2.jpg")); // 8
   images.push(loadImage("stupidshiteater.gif")); // 9
+
+  shapeType = [];
+  shapeType.push(sphere); // 0
+  shapeType.push(box); // 1
+  shapeType.push(torus); // 2
+  shapeType.push(plane); // 3
+  shapeType.push(cone); // 4
+  shapeType.push(cylinder); // 5
+
+
+  stuff = [];
+  stuff.push(images);
+  stuff.push(shapeType);
 }
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -48,7 +75,7 @@ function setup() {
 
 function draw() {
   
-  background("lavender");
+  background(backgroundColor);
   
   createSkyBox();
 
@@ -69,8 +96,8 @@ function createBox() {
       push();
       for (let l = 0; l < gridSize; l++) {
         translate(0, boxGap+boxSize, 0);
-        texture(images[imagenum]);
-        box(boxSize, boxSize, boxSize);
+        texture(stuff[0][imagenum]);
+        stuff[1][theShapenum](boxSize, boxSize, boxSize);
       }
       pop();
       translate(0, 0, boxGap+boxSize);
@@ -132,6 +159,7 @@ function camMovement() {
   if (keyIsDown(70)) {
     rotateY(millis() / 10);
   }
+  //h rotate
   if (keyIsDown(72)) {
     rotateY(millis() / -10);
   }
@@ -160,15 +188,15 @@ function camMovement() {
   if (keyIsDown(83)) {
     cam.move(0, 15, 0);
   }
-  // reset all
+  // z reset all
   if (keyIsDown(90)) {
-    boxGap = 0;
+    boxGap = -60;
   }
 }
 
 function createSkyBox() {
   //fill("red");
-  texture(images[SkyBoxnum]);
+  texture(stuff[0][SkyBoxnum]);
   box(7500, 7500, 7500);
 }
 
